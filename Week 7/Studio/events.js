27bf63eda1a77a02943fb49f61f6a5592823ce26
hkneller22 <139687145+hkneller22@users.noreply.github.com>
2024-09-12 17:42:46 +1000

    intro2js

---
+++ Week 7/Studio/events.js
@@ -0,0 +1,34 @@
+// Function declarations
+
+function incrementCounter() // function called on button click
+{
+    var spanElement = document.getElementById('counter');
+    var counterVar = spanElement.getAttribute('numberOfClicks');    // retrieve stuff
+
+    counterVar++;   // increment click counter
+
+    spanElement.innerText = counterVar; // set inner text to counter
+    spanElement.setAttribute('numberOfClicks', counterVar); // save number of clicks to attribute
+
+}
+
+
+function toggleVisibility()
+{
+    var chartImage = document.getElementById('chart1');
+
+    if (chartImage.style.display === 'none')
+    {
+        chartImage.style.display = 'block';
+    }
+    else    
+    {
+        chartImage.style.display = 'none'; 
+    }
+}
+
+
+
+// Script calls
+document.getElementById("incrementButton").addEventListener('click', incrementCounter); // call incrementCounter on incrementButton click
+document.getElementById("chartToggle").addEventListener('click', toggleVisibility); 
