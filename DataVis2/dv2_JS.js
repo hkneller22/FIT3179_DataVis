a15cd5b043ca0b1cbcf3dba52c484acfd008dd9f
hkneller22 <139687145+hkneller22@users.noreply.github.com>
2024-09-22 20:55:13 +1000

    homework done

---
+++ DataVis2/dv2_JS.js
@@ -0,0 +1,4 @@
+var vg_1 = "homeworkMap.vg.json";
+vegaEmbed("#prop_sym_map", vg_1).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
